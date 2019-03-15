fde890bad68c6eb10db9004de9787cd1a200f364
alfarog1 <alfaro.geovanny@gmail.com>
2019-03-15 17:13:38 -0600

    Completed Story 1-3

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -3,17 +3,28 @@ import java.util.Scanner;
 public class CashRegister {
 
 	public static void main(String[] args) {
-		System.out.println("What is the price of the item? ");
+		System.out.print("What is the price of the item? ");
 		Scanner sc = new Scanner(System.in);
 		
 		double price = sc.nextDouble();
 		
 		
-		System.out.println("How much cash was tendered? ");	
+		System.out.print("How much cash was tendered? ");	
 		double tendered = sc.nextDouble();
+
+		
+		if (tendered < price) {
+			System.out.println("Sorry you gave to little, need more cash. ");
+		}
+		if (tendered == price) {
+			System.out.println("Thanks for the exact change. ");
+		}
+		else if (tendered > price){
+			System.out.println("Wait for your change. ");
+			
+		}
 		
 		
-		if ()
 		
 		
 		
