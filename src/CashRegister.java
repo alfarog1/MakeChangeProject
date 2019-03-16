301513ff73fa744093880572aa9f25c648d4c0cb
alfarog1 <alfaro.geovanny@gmail.com>
2019-03-15 18:04:36 -0600

    Got as far as giving 0 and  in change

--- src/CashRegister.java
+++ src/CashRegister.java
@@ -3,24 +3,52 @@ import java.util.Scanner;
 public class CashRegister {
 
 	public static void main(String[] args) {
-		System.out.print("What is the price of the item? ");
+		System.out.print("What is the price of the item? $");
 		Scanner sc = new Scanner(System.in);
 		
 		double price = sc.nextDouble();
 		
-		
-		System.out.print("How much cash was tendered? ");	
+		System.out.print("How much cash was tendered? $");	
 		double tendered = sc.nextDouble();
 
+		double change = tendered - price;
 		
 		if (tendered < price) {
 			System.out.println("Sorry you gave to little, need more cash. ");
 		}
 		if (tendered == price) {
-			System.out.println("Thanks for the exact change. ");
+			System.out.println("Thanks for the exact change. " );
 		}
 		else if (tendered > price){
-			System.out.println("Wait for your change. ");
+			
+			System.out.println("Wait for your change of $" + change);
+			//After prompting for the user for price and tendered, giving change here on
+			do { double remainder;
+				 double bill10 =10;
+				 double bill5 =5;
+				 double bill1 =1;
+				 double coin25 =0.25;
+				 double coin10 =0.10;
+				 double coin5 =0.05;
+				 double coin1 =0.01;
+				
+			//make an if statement if over $20 was given if () 
+			
+				if (tendered - price > bill10) {
+					System.out.println("You get one $10 bill ");
+				}
+				if (tendered - bill10 >= bill5) {
+					System.out.println("You get one $5 bill");
+				}
+				//Since the 
+				if ((tendered - (bill10 + bill5) >= bill1)) {
+					 
+					//if
+					System.out.println("You get "+ /*count +*/ "$1 bill");
+					 
+				}
+				
+			} while (tendered - price == 0);
 			
 		}
 		
